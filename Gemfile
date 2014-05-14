ca19370e8b385e45c7f012246af9a84da378d061
Quentin Devauchelle <quentin@devauchelle.eu>
2014-05-14 16:36:28 +0200

    Initial commit

---
+++ Gemfile
@@ -0,0 +1,13 @@
+source 'https://rubygems.org'
+
+gem 'rails', '3.2.16'
+gem 'pg'
+
+gem 'jquery-rails'
+gem 'bcrypt-ruby', '~> 3.0.0'
+
+# Gems used only for assets and not required
+# in production environments by default.
+group :assets do
+  gem 'uglifier', '>= 1.0.3'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BodyStatus::Application.config.secret_token = '3c72da3fb237727f28cefbd88cfdac13a361c910663b242dc65a2b5ce771125e7e2c9eaef4871e1602a334854ede33a36b242952b404d10f7bfb17d1b95da796'

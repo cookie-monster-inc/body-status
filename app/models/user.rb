8ffe5315abcc01446c1239fd6a62f871d80e05d2
Quentin Devauchelle <quentin@devauchelle.eu>
2014-05-14 19:17:07 +0200

    Make model tests pass

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,6 @@
 class User < ActiveRecord::Base
-  
+  has_secure_password
+  validates_presence_of :email, :first_name, :last_name, :password
+  validates_length_of :password, :within => 6..40
+  validates_format_of :email, :with => /^\w+[\.\w\-]*@\w+\.\w{2,5}$/
 end

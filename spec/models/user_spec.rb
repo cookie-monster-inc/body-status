63357c13e2de1932010d2d509603e07d5acb707f
Quentin Devauchelle <quentin@devauchelle.eu>
2014-05-14 19:15:51 +0200

    Add user model spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,14 @@
 require 'spec_helper'
 
 describe User do
-  it 'should exist'
+  it { should have_secure_password }
+  context('#validations') do 
+    it { should validate_presence_of :email }
+    it { should validate_presence_of :first_name }
+    it { should validate_presence_of :last_name }
+    it { should validate_presence_of :password }
+    it { should ensure_length_of(:password).is_at_least(6).is_at_most(40) }
+    it { should allow_value("test@test.com").for :email}
+    it { should_not allow_value("qlskr.com","teae@sa",12).for :email}
+  end
 end

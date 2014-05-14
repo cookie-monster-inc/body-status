bdfe5e5d38731f1ac8d73410fbec6d162872e7ec
Quentin Devauchelle <quentin@devauchelle.eu>
2014-05-14 19:35:25 +0200

    Add password_confirmation presence validation

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -7,6 +7,7 @@ describe User do
     it { should validate_presence_of :first_name }
     it { should validate_presence_of :last_name }
     it { should validate_presence_of :password }
+    it { should validate_presence_of :password_confirmation }
     it { should ensure_length_of(:password).is_at_least(6).is_at_most(40) }
     it { should allow_value("test@test.com").for :email}
     it { should_not allow_value("qlskr.com","teae@sa",12).for :email}

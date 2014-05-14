e0598030ce14cf8f7180f2ae3f3a401722d632db
Quentin Devauchelle <quentin@devauchelle.eu>
2014-05-14 19:21:35 +0200

    Add a test for Users#new

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -2,6 +2,9 @@ require 'spec_helper'
 
 describe UsersController do
   context '#new' do 
-    it 'should exist'
+    it 'should exist' do 
+      get :new
+      expect(response).to be_success
+    end
   end
 end

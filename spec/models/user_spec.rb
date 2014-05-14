dc248c9476a2bf8f58d154df2446cba55c7b6ddc
Quentin Devauchelle <quentin@devauchelle.eu>
2014-05-14 19:53:37 +0200

    Allow mass_assignment on user columns

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,6 +2,11 @@ require 'spec_helper'
 
 describe User do
   it { should have_secure_password }
+  it { should allow_mass_assignment_of(:first_name) }
+  it { should allow_mass_assignment_of(:last_name) }
+  it { should allow_mass_assignment_of(:email) }
+  it { should allow_mass_assignment_of(:password) }
+  it { should allow_mass_assignment_of(:password_confirmation) }
   context('#validations') do 
     it { should validate_presence_of :email }
     it { should validate_presence_of :first_name }

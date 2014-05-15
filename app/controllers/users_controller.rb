41f58bdbe8639683abd72a14ae2688e1c0ddedbc
Quentin Devauchelle <quentin@devauchelle.eu>
2014-05-15 14:31:29 +0200

    Make Users controllers tests pass

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,13 @@
 class UsersController < ApplicationController
+  def new
+  end
+
+  def create
+    user = User.new params[:user]
+    if user.save
+      redirect_to new_user_path
+    else
+      render "users/new"
+    end
+  end
 end

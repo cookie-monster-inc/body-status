aac8c0a347e04c86026331f3509f70e219c5d113
Quentin Devauchelle <quentin@devauchelle.eu>
2014-05-15 15:15:46 +0200

    Make feature tests pass

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,14 @@
 class UsersController < ApplicationController
   def new
+    @user = User.new
   end
 
   def create
-    user = User.new params[:user]
-    if user.save
+    @user = User.new params[:user]
+    if @user.save
       redirect_to new_user_path
     else
+      @alerts = @user.errors.full_messages
       render "users/new"
     end
   end

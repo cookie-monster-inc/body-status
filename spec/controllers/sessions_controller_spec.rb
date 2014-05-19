9533c7636bd64baee70f9537c529335ea7f8d40e
Quentin Devauchelle <quentin@devauchelle.eu>
2014-05-19 11:15:21 +0200

    Add test for the happy path of SessionsController#create

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -1,10 +1,29 @@
 require 'spec_helper'
 
 describe SessionsController do 
+  let!(:user) {
+    create :user
+  }
+  let(:valid_login_params){
+    {
+      :email => user.email,
+      :password => user.password
+    }
+  }
+
   context '#new' do
     it 'should exist' do
       get :new
       expect(response).to be_success
+    end
+  end
+
+  context '#create' do 
+    it 'should log an user in' do 
+      expect {
+        post :create, valid_login_params
+        expect(response).to be_redirect
+      }.to change { request.session[:user_id] }.to user.id
     end
   end
 end

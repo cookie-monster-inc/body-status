36baa2bf8ea16b70e65570de830423c9c1b9cc01
Quentin Devauchelle <quentin@devauchelle.eu>
2014-05-19 11:18:13 +0200

    Make test for the unhappy path of SessionsController#create

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -19,11 +19,21 @@ describe SessionsController do
   end
 
   context '#create' do 
-    it 'should log an user in' do 
-      expect {
-        post :create, valid_login_params
-        expect(response).to be_redirect
-      }.to change { request.session[:user_id] }.to user.id
+    context 'with valid data' do
+      it 'should log an user in' do 
+        expect {
+          post :create, valid_login_params
+          expect(response).to be_redirect
+        }.to change { request.session[:user_id] }.to user.id
+      end
+    end
+    context 'with invalid data' do 
+      it 'should not log me in or redirect' do 
+        expect { 
+          post :create, {}
+          expect(response).not_to be_redirect
+        }.not_to change { request.session[:user_id] }
+      end
     end
   end
 end

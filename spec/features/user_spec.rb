2454d664a54c954c49f49b27b3498ab5b5d54400
Quentin Devauchelle <quentin@devauchelle.eu>
2014-05-21 16:50:55 +0200

    Remove not needed test. It is already tested in controller test

--- spec/features/user_spec.rb
+++ spec/features/user_spec.rb
@@ -8,35 +8,17 @@ describe "Users", :js => true do
     create :user
   }
   describe "User creation" do
-    context "With valid data" do 
-      it "should create a user" do
-        visit root_path
-        click_on "Register"
-        expect{
-          fill_in "First name", with: user_attr[:first_name]
-          fill_in "Last name", with: user_attr[:last_name]
-          fill_in "Email", with: user_attr[:email]
-          fill_in "Password", with: user_attr[:password]
-          fill_in "Password confirmation", with: user_attr[:password_confirmation]
-          click_on "Create User"
-          }.to change{ User.count }.by 1
-      end
-    end
-    context "With invalid data" do
-      it "should not create a user and print errors" do
-        # This is the only way to pass the html5 validation
-        visit root_path
-        click_on "Register"
-        expect{
-          fill_in "First name", with: attributes_for(:user)[:first_name]
-          fill_in "Last name", with: attributes_for(:user)[:last_name]
-          fill_in "Email", with: attributes_for(:user)[:email]
-          fill_in "Password", with: 'password'
-          fill_in "Password confirmation", with: 'password2'
-          click_on "Create User"
-          }.not_to change{ User.count }
-        expect(page).to have_content "Password doesn't match confirmation"
-      end
+    it "should create a user" do
+      visit root_path
+      click_on "Register"
+      expect{
+        fill_in "First name", with: user_attr[:first_name]
+        fill_in "Last name", with: user_attr[:last_name]
+        fill_in "Email", with: user_attr[:email]
+        fill_in "Password", with: user_attr[:password]
+        fill_in "Password confirmation", with: user_attr[:password_confirmation]
+        click_on "Create User"
+        }.to change{ User.count }.by 1
     end
   end
 
